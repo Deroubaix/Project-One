84f98c7e3279449797a9ff858d889cfa6f185dcf
Marisha Deroubaix <deroubaix.marisha@gmail.com>
2023-02-02 12:00:58 +0000

    animations, score

--- script/game.js
+++ script/game.js
@@ -11,13 +11,116 @@ class Game {
     this.enemies = [];
     this.score = 0;
     this.lives = 5;
-    this.livesImg = new Image();
-    this.livesImg.src = "/docs/assets/images/life.png";
     this.backgroundImage = new Image();
-  }
+
+      const img0 = new Image();
+      const img1 = new Image();
+      const img2 = new Image();
+      const img3 = new Image();
+      const img4 = new Image();
+      const img5 = new Image();
+      const img6 = new Image();
+      const img7 = new Image();
+      const img8 = new Image();
+      const img9 = new Image();
+      const img10 = new Image();
+      const img11 = new Image();
+      const img12 = new Image();
+      const img13 = new Image();
+      const img14 = new Image();
+      const img15 = new Image();
+      const img16 = new Image();
+      const img17 = new Image();
+      const img18 = new Image();
+      const img19 = new Image();
+      const img20 = new Image();
+      const img21 = new Image();
+      const img22 = new Image();
+      const img23 = new Image();
+      
+      img0.addEventListener("load", () => {})
+      img0.src = "/docs/assets/images/heart/frame_00_delay-0.04s.gif";
+      img1.addEventListener("load", () => {})
+      img1.src = "/docs/assets/images/heart/frame_01_delay-0.04s.gif";
+      img2.addEventListener("load", () => {})
+      img2.src = "/docs/assets/images/heart/frame_02_delay-0.04s.gif";
+      img3.addEventListener("load", () => {})
+      img3.src = "/docs/assets/images/heart/frame_03_delay-0.04s.gif";
+      img4.addEventListener("load", () => {})
+      img4.src = "/docs/assets/images/heart/frame_04_delay-0.04s.gif";
+      img5.addEventListener("load", () => {})
+      img5.src = "/docs/assets/images/heart/frame_05_delay-0.04s.gif";
+      img6.addEventListener("load", () => {})
+      img6.src = "/docs/assets/images/heart/frame_06_delay-0.04s.gif";
+      img7.addEventListener("load", () => {})
+      img7.src = "/docs/assets/images/heart/frame_07_delay-0.04s.gif";
+      img8.addEventListener("load", () => {})
+      img8.src = "/docs/assets/images/heart/frame_08_delay-0.04s.gif";
+      img9.addEventListener("load", () => {})
+      img9.src = "/docs/assets/images/heart/frame_09_delay-0.04s.gif";
+      img10.addEventListener("load", () => {})
+      img10.src = "/docs/assets/images/heart/frame_10_delay-0.04s.gif";
+      img11.addEventListener("load", () => {})
+      img11.src = "/docs/assets/images/heart/frame_11_delay-0.04s.gif";
+      img12.addEventListener("load", () => {})
+      img12.src = "/docs/assets/images/heart/frame_12_delay-0.04s.gif";
+      img13.addEventListener("load", () => {})
+      img13.src = "/docs/assets/images/heart/frame_13_delay-0.04s.gif";
+      img14.addEventListener("load", () => {})
+      img14.src = "/docs/assets/images/heart/frame_14_delay-0.04s.gif";
+      img15.addEventListener("load", () => {})
+      img15.src = "/docs/assets/images/heart/frame_15_delay-0.04s.gif";
+      img16.addEventListener("load", () => {})
+      img16.src = "/docs/assets/images/heart/frame_16_delay-0.04s.gif";
+      img17.addEventListener("load", () => {})
+      img17.src = "/docs/assets/images/heart/frame_17_delay-0.04s.gif";
+      img18.addEventListener("load", () => {})
+      img18.src = "/docs/assets/images/heart/frame_18_delay-0.04s.gif";
+      img19.addEventListener("load", () => {})
+      img19.src = "/docs/assets/images/heart/frame_19_delay-0.04s.gif";
+      img20.addEventListener("load", () => {})
+      img20.src = "/docs/assets/images/heart/frame_20_delay-0.04s.gif";
+      img21.addEventListener("load", () => {})
+      img21.src = "/docs/assets/images/heart/frame_21_delay-0.04s.gif";
+      img22.addEventListener("load", () => {})
+      img22.src = "/docs/assets/images/heart/frame_22_delay-0.04s.gif";
+      img23.addEventListener("load", () => {})
+      img23.src = "/docs/assets/images/heart/frame_23_delay-0.04s.gif";
+  
+      this.img = img1;
+      this.images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12,
+                     img13, img14, img15, img16, img17, img18,img19,img20,img21,img22,img23]
+    }
+  
+    drawLives(frames) {
+      this.img = this.images[Math.floor(frames % 30 / 3.75)]
+      if (this.lives === 5) {
+        this.ctx.drawImage(this.img, 10, 30);
+        this.ctx.drawImage(this.img, 40, 30);
+        this.ctx.drawImage(this.img, 70, 30);
+        this.ctx.drawImage(this.img, 100, 30);
+        this.ctx.drawImage(this.img, 130, 30);
+      } else if (this.lives === 4) {
+        this.ctx.drawImage(this.img, 10, 30);
+        this.ctx.drawImage(this.img, 40, 30);
+        this.ctx.drawImage(this.img, 70, 30);
+        this.ctx.drawImage(this.img, 100, 30);
+      } else if (this.lives === 3) {
+        this.ctx.drawImage(this.img, 10, 30);
+        this.ctx.drawImage(this.img, 40, 30);
+        this.ctx.drawImage(this.img, 70, 30);
+      } else if (this.lives === 2) {
+        this.ctx.drawImage(this.img, 10, 30);
+        this.ctx.drawImage(this.img, 40, 30);
+      } else if (this.lives === 1) {
+        this.ctx.drawImage(this.img, 10, 30);
+      }
+    }
+  
 
   start() {
     this.intervalId = setInterval(this.update, 1000 / 60);
+    this.drawScore()
   }
 
   update = () => {
@@ -30,7 +133,7 @@ class Game {
     this.checkGameOver();
     this.drawScore();
     this.updateScore();
-    this.drawLives();
+    this.drawLives(this.frames);
   };
 
   stop() {
@@ -44,35 +147,10 @@ class Game {
     this.ctx.drawImage(this.backgroundImage, 0, 0, 1200, 450);
   }
 
-  drawLives() {
-    if (this.lives === 5) {
-      this.ctx.drawImage(this.livesImg, 10, 30);
-      this.ctx.drawImage(this.livesImg, 40, 30);
-      this.ctx.drawImage(this.livesImg, 70, 30);
-      this.ctx.drawImage(this.livesImg, 100, 30);
-      this.ctx.drawImage(this.livesImg, 130, 30);
-    } else if (this.lives === 4) {
-      this.ctx.drawImage(this.livesImg, 10, 30);
-      this.ctx.drawImage(this.livesImg, 40, 30);
-      this.ctx.drawImage(this.livesImg, 70, 30);
-      this.ctx.drawImage(this.livesImg, 100, 30);
-    } else if (this.lives === 3) {
-      this.ctx.drawImage(this.livesImg, 10, 30);
-      this.ctx.drawImage(this.livesImg, 40, 30);
-      this.ctx.drawImage(this.livesImg, 70, 30);
-    } else if (this.lives === 2) {
-      this.ctx.drawImage(this.livesImg, 10, 30);
-      this.ctx.drawImage(this.livesImg, 40, 30);
-    } else if (this.lives === 1) {
-      this.ctx.drawImage(this.livesImg, 10, 30);
-    } else {
-    }
-  }
-
   drawScore() {
     ctx.font = "20px Helvetica";
-    ctx.fillStyle = "black";
-    ctx.fillText(`Score: ${this.score}`, 12, 20);
+    ctx.fillStyle = "white";
+    ctx.fillText(`Score: ${this.score}`, 60, 20);
   }
 
   screenScore() {
@@ -82,6 +160,10 @@ class Game {
     ctx.font = "40px Helvetica";
     ctx.textAlign = "center";
     ctx.fillText("Your Score: " + this.score, canvas.width / 2, canvas.height / 2 + 30);
+
+    if (this.screenScore() === true) {
+      this.drawScore() === false
+    } 
   }
 
   updateScore() {
